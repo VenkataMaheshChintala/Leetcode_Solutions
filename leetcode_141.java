c5b81cd677658c499e2a5c0c65d288bff8c71bb3
Venkata Mahesh Chintala <venkatamaheshchintala@gmail.com>
2024-09-15 18:01:27 +0530

    new file

---
+++ leetcode_141.java
@@ -0,0 +1,23 @@
+public class leetcode_141 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if(head==null) return false;
+            ListNode fast = head;
+            ListNode slow = head;
+            while(fast.next!=null && fast!=null && fast.next.next!=null) {
+                fast = fast.next.next;
+                slow = slow.next;
+                if(fast==slow) return true;
+            }
+            return false;
+        }
+    }
+}
