9fc7bcb07f444f819db011262492bffa64ce1740
Venkata Mahesh Chintala <venkatamaheshchintala@gmail.com>
2026-02-24 23:08:40 +0530

    new files

---
+++ leetcode_739.java
@@ -0,0 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class leetcode_739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>(); 
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
